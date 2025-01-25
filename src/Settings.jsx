68b60568518619772e5d5735ef04a938e6d6d012
wuh95 <117808968+hongmiao-wu@users.noreply.github.com>
2025-01-25 17:21:19 +0100

    added settings styling

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,24 +1,53 @@
+import './Settings.css'
+
 function Settings({ settings, setSettings }) {
-    const handleChange = (e) => {
-      const { name, value } = e.target
-      setSettings((prevSettings) => ({
-        ...prevSettings,
-        [name]: Number(value),
-      }))
-    }
-  
-    return (
-      <div className="settings">
-        <label>
-          Pomodoro Duration:
-          <input type="number" name="pomodoro" value={settings.pomodoro} onChange={handleChange} />
-        </label>
-        <label>
-          Break Duration:
-          <input type="number" name="break" value={settings.break} onChange={handleChange} />
-        </label>
-      </div>
-    )
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setSettings((prevSettings) => ({
+      ...prevSettings,
+      [name]: Number(value),
+    }))
   }
-  
-  export default Settings
+
+  return (
+    <div className="settings">
+      <div className="settings-row">
+        <div className="settings-group">
+          <label htmlFor="pomodoro">Pomodoro</label>
+          <input 
+            type="number" 
+            id="pomodoro" 
+            name="pomodoro" 
+            value={settings.pomodoro} 
+            onChange={handleChange} 
+            placeholder="25"
+          />
+        </div>
+        <div className="settings-group">
+          <label htmlFor="break">Short Break</label>
+          <input 
+            type="number" 
+            id="break" 
+            name="break" 
+            value={settings.break} 
+            onChange={handleChange} 
+            placeholder="5"
+          />
+        </div>
+        <div className="settings-group">
+          <label htmlFor="longBreak">Long Break</label>
+          <input 
+            type="number" 
+            id="longBreak" 
+            name="longBreak" 
+            value={settings.longBreak} 
+            onChange={handleChange} 
+            placeholder="15"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Settings
